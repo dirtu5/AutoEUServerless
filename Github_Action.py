c0005574f004e3955de83d00733ba889d576e69b
dirtu5 <139054771+dirtu5@users.noreply.github.com>
2025-01-30 15:27:58 +0800

    Update Github_Action.py

--- Github_Action.py
+++ Github_Action.py
@@ -17,15 +17,15 @@ import requests
 from bs4 import BeautifulSoup
 
 # 账户信息：用户名和密码
-USERNAME = os.getenv('chongshang394929824@gmail.com')  # 填写用户名或邮箱
-PASSWORD = os.getenv('12345678o')  # 填写密码
+USERNAME = os.getenv('EUSERV_USERNAME')  # 填写用户名或邮箱
+PASSWORD = os.getenv('EUSERV_PASSWORD')  # 填写密码
 
 # TrueCaptcha API 配置
 TRUECAPTCHA_USERID = os.getenv('TRUECAPTCHA_USERID')
 TRUECAPTCHA_APIKEY = os.getenv('TRUECAPTCHA_APIKEY')
 
 # Mailparser 配置
-MAILPARSER_DOWNLOAD_URL_ID = os.getenv('yoxsebdq')
+MAILPARSER_DOWNLOAD_URL_ID = os.getenv('MAILPARSER_DOWNLOAD_URL_ID')
 MAILPARSER_DOWNLOAD_BASE_URL = "https://files.mailparser.io/d/"
 
 # Telegram Bot 推送配置
